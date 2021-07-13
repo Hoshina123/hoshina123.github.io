7fee074345daa44a70589e8638f5e4e26a64f226
Administrator-user <xzx7131@outlook.com>
2021-07-13 14:43:30 +0800

    Update the contributions page

--- Scripts/createCalendar.js
+++ Scripts/createCalendar.js
@@ -1,9 +1,11 @@
 window.onload = function(){
-    let weekIndex = 1;
-    let weekX = 0;
-    let contributions = document.getElementById("contributionsCalendar");
-    let lastDays = 2;
-    for (let w=0;w<51;w++){
+    // create the calendar
+    var weekIndex = 1;
+    var weekX = 0;
+    var contributions = document.getElementById("contributionsCalendar");
+    const deletes = 4;
+
+    for (let w=0;w<52;w++){
         weekIndex++;
         weekX += 15;
         let week = document.getElementById("week1");
@@ -12,12 +14,56 @@ window.onload = function(){
         weekClone.setAttribute("transform","translate("+weekX+",0)");
         contributions.appendChild(weekClone);
     }
-    
-    let lastWeek = document.createElement("g");
-    lastWeek.setAttribute("id","week53");
-    lastWeek.setAttribute("transform","translate("+(weekX+15)+",0)");
-    contributions.appendChild(lastWeek)
-    for (let i=0;i<lastDays;i++){
-        let day = document.createElement("rect");
+    lastWeek = document.getElementById("week53");
+    var dayY = 0;
+    var days = lastWeek.children;
+    for (let i=deletes-1;i>=0;i--){
+        lastWeek.removeChild(days[i]);
+    }
+    for (let i=0;i<7-deletes;i++){
+        lastWeek.children[i].setAttribute("y",dayY);
+        dayY += 15;
+    }
+
+    // set ID
+    days = document.getElementsByClassName("contributions_day");
+    for (let i=1;i<=367;i++){
+        days[i-1].setAttribute("id",i);
+    }
+
+    // set contribution level
+    const levels = [0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,1,0,0,0,0,2,2,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,1,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,1,0,0,2,0,
+        2,0,2,1,0,0,1,1,1,1,1,1,1,1,
+        1,1,0,0,0,1,1,1,0,1,1,1,1,1,
+        1,0,0,0,0,1,0,1,0,1,0,2,1,0,
+        1,0,0,0,1,0,0,0,0,0,0,0,0,0,
+        0,0,2,0,0,0,0,0,0,0,0,0,2,1,
+        2,4,1,2,2,1,0,0,1,1,1,2,0,1,
+        1,0,0,1,1,1,0,1,1,1,1,1,1,0,
+        1,0,1,2,0];
+    levelColors = ["#ececec","#86ee97","#49e673","#00cc52","#00a039"];
+    for (let i=1;i<=367;i++){
+        let item = document.getElementById(i);
+        item.setAttribute("level",levels[i-1]);
+        let index = Number(item.getAttribute("level"));
+        item.setAttribute("fill",levelColors[index]);
     }
 }
